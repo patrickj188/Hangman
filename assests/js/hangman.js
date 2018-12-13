78efbaa424a2733638e7d68c5ab75de33748c8a8
patrickj188@gmail.com <Patrickj188@gmail.com>
2018-12-13 11:07:15 -0600

    v4

--- assests/js/hangman.js
+++ assests/js/hangman.js
@@ -1,7 +1,3 @@
-
-
-
-
 function getItem() {
     var array = [
         {
@@ -16,7 +12,7 @@ function getItem() {
 
         {
             answer: "jaws",
-            hint: "Ok, ok, You’re Gonna Need A Bigger Boat…"
+            hint: "You’re Gonna Need A Bigger Boat…"
         },
 
         {
@@ -53,7 +49,9 @@ function getItem() {
             answer: "psycho",
             hint: "A boy's best friend is his mother.",
         }
+
     ]
+
 
     var randomNumber = Math.floor(Math.random() * array.length);
     return array[randomNumber];
@@ -63,69 +61,80 @@ function getItem() {
 };
 
 
-
 var randomThing = getItem()
 // console.log(randomThing.hint);
 // console.log(randomThing.answer);
-document.getElementById("hint-text").innerHTML = ("Here is your hint.... " + randomThing.hint);
-// document.getElementById("blankspaces-text").innerHTML = ("your word: " + blankSpacesAnswer.join(" "));
-// document.getElementById("usser-guesses-text").innerHTML = ("your guesses: ");
-
-
+document.getElementById("hint-text").innerHTML = ("Here is your hint.... " + randomThing.hint.italics());
 
 var blankSpacesAnswer = [];
+var blanksAndSuccesses = document.getElementById("blankspaces-text");
 for (var i = 0; i < randomThing.answer.length; i++) {
     blankSpacesAnswer[i] = "_";
     console.log(blankSpacesAnswer);
-    document.getElementById("blankspaces-text").innerHTML = ("Your word:   " + blankSpacesAnswer.join(" "));
+    blanksAndSuccesses.innerHTML = ("Your word: " + blankSpacesAnswer.join(" "));
 };
 
 var currentWord;
 var guessingWord = [];
 var winCounter = 0;
 var lossCounter = 0;
-var numGuesses = 15;
+var numGuesses = 12;
 var letterGuessed = "";
 var wrongGuesses = [];
-var blanksAndSuccesses = [];
-
 
 var remainingLetters = randomThing.answer.length;
-
-
-//game loop 
 
 document.onkeyup = function (event) {
     var userGuess = event.key;
     console.log(userGuess);
-
-
-
+    var answerLetterArray = randomThing.answer.split("");
+    guessingWord.length = answerLetterArray.length;
+    console.log(answerLetterArray);
 
     if (remainingLetters > 0) {
-        //show player progress
         var guessesEl = document.getElementById("blankspaces-text")
         guessesEl.innerHTML = blankSpacesAnswer.join(" ")
 
-        if (userGuess.length !== 1) {
-        } else {
-            for (var j = 0; j < randomThing.answer.length; j++) {
-                if (randomThing.answer[j] === userGuess) {
-                    blankSpacesAnswer[j] = userGuess;
-                    remainingLetters--;
-                    
-                    guessesEl.innerHTML = ("your word:   " + blankSpacesAnswer.join(" "));
-                } else if (randomThing.answer[j] !== userGuess); {
-                    wrongGuesses = userGuess;
-                    numGuesses--;
-                    document.getElementById("usser-guesses-text").textContent = ("your guesses: " + wrongGuesses);
-                    document.getElementById("guesses-left").textContent = ("Number of guesses: " + numGuesses);
 
+        var foundIndex;
+        for (var i = 0; i < answerLetterArray.length; i++) {
+            console.log(answerLetterArray[i]);
 
-                } 
-                
+            if (userGuess === answerLetterArray[i]) {
+                blankSpacesAnswer[i] = userGuess
+
             }
+
+           else if (answerLetterArray[i]!== userGuess){
+                numGuesses--;
+            }
+
+
+            blanksAndSuccesses.innerHTML = ("your word:   " + blankSpacesAnswer.join(" "));
+            document.getElementById("guesses-left").textContent = ("Number of guesses: " + numGuesses);
+            document.getElementById("usser-guesses-text").textContent = ("Your guesses: " + userGuess);
+
+
+
+            // blanksAndSuccesses.innerHTML = ("your word:   " + answerLetterArray.join(""));
         }
+
+
+
+        // if (foundIndex !== null) {
+        //     blankSpacesAnswer[foundIndex] = userGuess;
+        //     remainingLetters--;
+        // } else {
+        //     wrongGuesses.push(userGuess);
+        // }
+        // blanksAndSuccesses.innerHTML = ("your word:   " + blankSpacesAnswer.join(" "));
+        // document.getElementById("usser-guesses-text").textContent = ("your guesses: " + wrongGuesses);
+        // document.getElementById("guesses-left").textContent = ("Number of guesses: " + numGuesses);
+        // numGuesses--;
+
     };
 
+
+
+    
 };
